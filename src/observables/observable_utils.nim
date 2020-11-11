2ef65af62818f723dcb7d43fec733a8f5748c1ee
Kristian Hasselknippe <kristian_hasselknippe@outlook.com>
2020-11-11 10:52:23 +0100

    Added some utility procs

--- src/observables/observable_utils.nim
+++ src/observables/observable_utils.nim
@@ -1,3 +1,4 @@
+import options
 import ./types
 import ./observables
 
@@ -9,4 +10,25 @@ proc choose*[T](self: Observable[bool], onTrue: T, onFalse: T): Observable[T] =
         onTrue
       else:
         onFalse
+  )
+
+proc unwrap*[T](self: Observable[Option[T]]): Observable[T] =
+  self.filter(
+    proc(x: Option[T]): bool =
+      x.isSome()
+  ).map(
+    proc(x: Option[T]): T =
+      x.get()
+  )
+
+proc unwrap*[T](self: Observable[seq[Option[T]]]): Observable[seq[T]] =
+  self.map(
+    proc(x: seq[Option[T]]): seq[T] =
+      x.filter(
+        proc(o: Option[T]): bool =
+          o.isSome()
+      ).map(
+        proc(o: Option[T]): T =
+          o.get()
+      )
   )
